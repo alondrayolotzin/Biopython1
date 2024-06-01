7cb7d0e454a18566ed24de4d0223eaa294668cbc
alondrayolotzin <160920865+alondrayolotzin@users.noreply.github.com>
2024-05-31 18:24:55 -0600

    cambios en comentarios de codigo

--- Marcoslectura_t8/seq_frames.py
+++ Marcoslectura_t8/seq_frames.py
@@ -1,9 +1,7 @@
 from Bio import SeqIO
 
-def encontrar_codones(marco, secuencia):
-    """
-    Encuentra todos los codones en el marco de lectura especificado.
-    """
+def encontrar_codones(marco, secuencia): # a partir de cada marco de lectura se determinan los codones correspondientes 
+    
     codones = []
     for i in range(marco, len(secuencia), 3):
         codon = secuencia[i:i+3]
@@ -11,19 +9,15 @@ def encontrar_codones(marco, secuencia):
             codones.append(codon)
     return codones
 
-def escribir_codones_a_archivo(marco, secuencia, archivo_salida, hebra):
-    """
-    Escribe los codones en formato FASTA en el archivo especificado.
-    """
+def escribir_codones_a_archivo(marco, secuencia, archivo_salida, hebra): # se pasan codones en formato FASTA
+
     codones = encontrar_codones(marco, secuencia)
     with open(archivo_salida, 'w') as archivo:
         archivo.write(f">Frame_{marco+1}_{hebra}\n")
         archivo.write(" ".join(codones) + "\n")
 
-def procesar_marcos(secuencia, nombre_base_archivo, hebra, secuencia_id):
-    """
-    Procesa los 6 marcos de lectura y escribe los codones en archivos separados.
-    """
+def procesar_marcos(secuencia, nombre_base_archivo, hebra, secuencia_id): # Se procesan los marcos de lectura 
+  
     for marco in range(3):
         archivo_salida = f"{nombre_base_archivo}_seq{secuencia_id}_Frame{marco+1}_{hebra}.fa"
         escribir_codones_a_archivo(marco, secuencia, archivo_salida, hebra)
@@ -37,8 +31,8 @@ for i, registro in enumerate(secuencias):
     secuencia = str(registro.seq)
     secuencia_reversa = secuencia[::-1]
 
-    # Procesar los marcos de lectura para la hebra original
+    # Procesar los marcos de lectura para la hebra en sentido original
     procesar_marcos(secuencia, "Frame", "directa", i+1)
 
-    # Procesar los marcos de lectura para la hebra inversa
+    # Procesar los marcos de lectura para la hebra en sentido inverso
     procesar_marcos(secuencia_reversa, "Frame", "inversa", i+1)
